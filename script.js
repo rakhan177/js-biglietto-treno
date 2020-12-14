b23b7303653d8a786df31f2f02e5a9a6db37c798
Matteo <metboolean@gmail.com>
2020-12-14 17:17:45 +0100

    pulizia

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ alert('Buongiorno caro utente.')
 var prezzoKilometro = 0.21;
 console.log('prezzokilometro' + prezzoKilometro);
 
-var kilometriUtente = prompt('Quanti chilometri desidera fare?');
+var kilometriUtente = parseInt(prompt('Quanti chilometri desidera fare?'));
 console.log('kilometriUtente' + kilometriUtente);
 
 var prezzoUniversale = (prezzoKilometro * kilometriUtente);
@@ -13,18 +13,21 @@ console.log('prezzoUniversale' + prezzoUniversale);
 console.log('scontoMinori' + scontoMinori);
 console.log('scontoOver' + scontoOver);
 
-var etaUtente = prompt('Quanti anni ha?');
+var etaUtente = parseInt(prompt('Quanti anni ha?'));
 console.log('etaUtente' + etaUtente);
+
+var stampPrice = document.getElementById('prezzo');
+
+if (isNaN(kilometriUtente) || isNaN(etaUtente)) {
+  alert('Deve specificare un numero.');
+}
 
 if(etaUtente < 19){
   var prezzoMinori = prezzoUniversale - scontoMinori;
-  var stampMinori = document.getElementById('prezzo');
-  stampMinori.innerText = 'Perfetto,mi faccia fare un rapido calcolo...mhhhh... sono' +" "+ prezzoMinori +" "+ 'euro.';
+  stampPrice.innerText = 'Perfetto,mi faccia fare un rapido calcolo...mhhhh... sono' +" "+ prezzoMinori.toFixed(2) +" "+ 'euro.';
 }else if(etaUtente > 64){
   var prezzoOver = prezzoUniversale - scontoOver;
-  var stampOver = document.getElementById('prezzo');
-  stampOver.innerText = 'Perfetto,mi faccia fare un rapido calcolo...mhhhh... sono' +" "+ prezzoOver +" "+ 'euro.';
+  stampPrice.innerText = 'Perfetto,mi faccia fare un rapido calcolo...mhhhh... sono' +" "+ prezzoOver.toFixed(2) +" "+ 'euro.';
 }else{
-  var stampOther = document.getElementById('prezzo');
-  stampOther.innerText = 'Perfetto, sono' +" "+ prezzoUniversale +" "+ 'euro.';
+  stampPrice.innerText = 'Perfetto, sono' +" "+ prezzoUniversale.toFixed(2) +" "+ 'euro.';
 }
